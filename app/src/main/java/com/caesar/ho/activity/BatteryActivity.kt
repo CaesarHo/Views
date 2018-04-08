79b6b24bb02e6cac57898ad15279bbf2f45bee2b
Caesar Ho <332609358@qq.com>
2018-04-08 13:49:54 +0800

    20180408 1.新增自定义电池

---
+++ app/src/main/java/com/caesar/ho/activity/BatteryActivity.kt
@@ -0,0 +1,53 @@
+package com.caesar.ho.activity
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import com.caesar.ho.R
+import com.caesar.ho.view.BatteryView
+import java.util.*
+
+
+class BatteryActivity : AppCompatActivity() {
+
+    private var horizontalBattery: BatteryView? = null
+    private var verticalBattery:BatteryView? = null
+    private var power: Int = 0
+    private var index: Int = 5;
+    private val mHandler = object : Handler() {
+        override fun handleMessage(msg: Message?) {
+            super.handleMessage(msg)
+            when (msg?.what) {
+                0 -> {
+                    index += power+index
+                    horizontalBattery!!.power = index
+                    if (power == 100) {
+                        power = 0
+                    }
+                }
+                else -> {
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_battery)
+
+        horizontalBattery = findViewById(R.id.horizontalBattery) as BatteryView
+        verticalBattery = findViewById(R.id.verticalBattery) as BatteryView
+
+        verticalBattery!!.setColor(Color.BLACK)
+        verticalBattery!!.setPower(85)
+
+        Timer().schedule(object : TimerTask() {
+            override
+            fun run() {
+                mHandler.sendEmptyMessage(0)
+            }
+        }, 0, 100)
+    }
+}
